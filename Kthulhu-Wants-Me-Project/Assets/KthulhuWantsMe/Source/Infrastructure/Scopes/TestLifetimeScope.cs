d46b1db9a64da4b8d67ac1aefd4e600b59ad3f6a
quizandpuzzle <quizandpuzzle@gmail.com>
2023-08-25 10:48:08 +0300

    SetUp Tentacle IK

---
+++ Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Infrastructure/Scopes/TestLifetimeScope.cs
@@ -0,0 +1,18 @@
+using KthulhuWantsMe.Source.Gameplay.Player;
+using KthulhuWantsMe.Source.Infrastructure.Installers;
+using VContainer;
+using VContainer.Unity;
+
+namespace KthulhuWantsMe.Source.Infrastructure.Scopes
+{
+    public class TestLifetimeScope : LifetimeScope
+    {
+        public PlayerSpawnPoint PlayerSpawnPoint;
+        
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Install(new ServicesInstaller());
+            builder.Install(new GameInstaller(PlayerSpawnPoint));
+        }
+    }
+}
