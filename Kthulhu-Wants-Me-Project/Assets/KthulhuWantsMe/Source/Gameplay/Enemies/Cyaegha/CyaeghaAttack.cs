78b1f24b048d34ac0d1ec73679a4d5be24e7474e
Alex Rogatch <quizandpuzzle@gmail.com>
2023-09-19 20:05:42 +0300

    Fix cyaegha not rotating towards target on attack

--- Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Gameplay/Enemies/Cyaegha/CyaeghaAttack.cs
+++ Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Gameplay/Enemies/Cyaegha/CyaeghaAttack.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using KthulhuWantsMe.Source.Gameplay.DamageSystem;
 using KthulhuWantsMe.Source.Gameplay.Enemies.Yith;
 using KthulhuWantsMe.Source.Gameplay.Entity;
@@ -7,6 +8,7 @@ using KthulhuWantsMe.Source.Infrastructure.Services;
 using KthulhuWantsMe.Source.Infrastructure.Services.DataProviders;
 using MoreMountains.Feedbacks;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Serialization;
 using VContainer;
 
@@ -20,6 +22,7 @@ namespace KthulhuWantsMe.Source.Gameplay.Enemies.Cyaegha
         public AnimationCurve HeightCurve;
 
         [SerializeField] private Enemy _enemy;
+        [SerializeField] private NavMeshAgent _cyaeghaNavMesh;
         
         private float _attackCooldown;
         private bool _isAttacking;
@@ -39,6 +42,8 @@ namespace KthulhuWantsMe.Source.Gameplay.Enemies.Cyaegha
             Vector3 jumpStartPos = transform.position;
             Vector3 dest = lastPlayerPosition;
             _isAttacking = true;
+            
+            FaceTarget(lastPlayerPosition);
 
             for (float t = 0; t < 1; t += Time.deltaTime * 2f)
             {
@@ -46,6 +51,7 @@ namespace KthulhuWantsMe.Source.Gameplay.Enemies.Cyaegha
                                      + Vector3.up * HeightCurve.Evaluate(t);
                 yield return null;
             }
+            
 
             if (PhysicsUtility.HitFirst(transform,
                     AttackStartPoint(),
@@ -58,9 +64,29 @@ namespace KthulhuWantsMe.Source.Gameplay.Enemies.Cyaegha
 
             //NavMesh SamplePosition?
 
+            if (NavMesh.SamplePosition(lastPlayerPosition, out NavMeshHit hit,1f,_cyaeghaNavMesh.areaMask))
+            {
+                _cyaeghaNavMesh.Warp(hit.position);
+            }
+
             _isAttacking = false;
             _attackCooldown = 1f;
         }
+        private void FaceTarget(Vector3 destination)
+        {
+            Vector3 lookPos = destination - transform.position;
+            lookPos.y = 0;
+            Quaternion rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 3);  
+        }
+        
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward);
+        }
+
 
         public bool CanAttack()
         {
