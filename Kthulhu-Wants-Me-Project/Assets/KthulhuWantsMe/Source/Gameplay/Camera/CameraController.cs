7b1a0ece08c2427a7e370a8dfc38ef7e92fce124
Alex Rogatch <quizandpuzzle@gmail.com>
2023-10-03 09:00:56 +0300

    Fix fade object effect bug

--- Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Gameplay/Camera/CameraController.cs
+++ Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Gameplay/Camera/CameraController.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using KthulhuWantsMe.Source.Gameplay.Player;
+using KthulhuWantsMe.Source.Infrastructure.Services;
 using UnityEngine;
+using VContainer;
 
 namespace KthulhuWantsMe.Source.Gameplay.Camera
 {
@@ -20,6 +23,15 @@ namespace KthulhuWantsMe.Source.Gameplay.Camera
         [SerializeField] private CinemachineBrain _cinemachineBrain;
 
         private Renderer _inCameraView;
+
+        private RaycastHit[] _results = new RaycastHit[10];
+        private IGameFactory _gameFactory;
+
+        [Inject]
+        public void Construct(IGameFactory gameFactory)
+        {
+            _gameFactory = gameFactory;
+        }
 
         private void Start() =>
             Cursor.lockState = CursorLockMode.Confined;
@@ -41,48 +53,52 @@ namespace KthulhuWantsMe.Source.Gameplay.Camera
 
         private void FadeObjectsInView()
         {
-            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 1000f))
+            Vector3 dir = _gameFactory.Player.transform.position - transform.position;
+            Ray ray = new Ray(transform.position, dir.normalized);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 if (hit.transform.TryGetComponent(out Renderer rendererComponent))
                 {
-                    if (rendererComponent.material.renderQueue == (int)UnityEngine.Rendering.RenderQueue.Transparent && (_inCameraView == null ||hit.transform != _inCameraView.transform))
+                    if (rendererComponent.material.renderQueue == (int)UnityEngine.Rendering.RenderQueue.Transparent &&
+                        (_inCameraView == null || hit.transform != _inCameraView.transform))
                     {
                         StartCoroutine(DoFadeObject(rendererComponent));
-                    
+
                         _inCameraView = rendererComponent;
                     }
                 }
-                
-                if (_inCameraView != null && hit.transform != _inCameraView.transform )
+
+                if (_inCameraView != null && hit.transform != _inCameraView.transform)
                 {
                     StartCoroutine(DoUnFadeObject(_inCameraView));
                     _inCameraView = null;
                 }
-
             }
+          
         }
 
         private IEnumerator DoFadeObject(Renderer rendererComponent)
         {
             rendererComponent.material.SetFloat("_StencilMode", 1);
             rendererComponent.material.SetFloat("_StencilNo", 10);
-            for (float t = 0; t > -.75f; t-= Time.deltaTime*2f)
+            for (float t = 0; t > -.75f; t -= Time.deltaTime * 2f)
             {
                 rendererComponent.material.SetFloat("_Tweak_transparency", t);
                 yield return null;
             }
         }
-        
+
         private IEnumerator DoUnFadeObject(Renderer rendererComponent)
         {
-            
             rendererComponent.material.SetFloat("_StencilMode", 0);
             rendererComponent.material.SetFloat("_StencilNo", 1);
-            for (float t = rendererComponent.material.GetFloat("_Tweak_transparency"); t < 0; t+= Time.deltaTime*2f)
+            for (float t = rendererComponent.material.GetFloat("_Tweak_transparency"); t < 0; t += Time.deltaTime * 2f)
             {
                 rendererComponent.material.SetFloat("_Tweak_transparency", t);
                 yield return null;
             }
+
             rendererComponent.material.SetFloat("_Tweak_transparency", 0);
         }
     }
