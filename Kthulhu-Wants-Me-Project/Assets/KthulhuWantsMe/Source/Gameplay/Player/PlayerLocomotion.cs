e6f8bfffab3d7eaa2598eb2312e585e6e8f6ee70
Alex Rogatch <quizandpuzzle@gmail.com>
2023-10-17 23:08:30 +0300

    CleanUp

--- Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Gameplay/Player/PlayerLocomotion.cs
+++ Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Gameplay/Player/PlayerLocomotion.cs
@@ -1,8 +1,6 @@
-using System;
 using Freya;
 using KinematicCharacterController;
-using KthulhuWantsMe.Source.Gameplay.Camera;
-using KthulhuWantsMe.Source.Gameplay.Enemies.Yith;
+using KthulhuWantsMe.Source.Gameplay.Enemies.AI;
 using KthulhuWantsMe.Source.Gameplay.Player.AttackSystem;
 using KthulhuWantsMe.Source.Infrastructure.Services;
 using KthulhuWantsMe.Source.Infrastructure.Services.DataProviders;
@@ -35,7 +33,6 @@ namespace KthulhuWantsMe.Source.Gameplay.Player
         private IInputService _inputService;
         private PlayerConfiguration _playerConfig;
         private ICoroutineRunner _coroutineRunner;
-        private CinemachineCameraPanning _cameraPanningLogic;
 
         [Inject]
         public void Construct(IInputService inputService, IDataProvider dataProvider, ICoroutineRunner coroutineRunner)
