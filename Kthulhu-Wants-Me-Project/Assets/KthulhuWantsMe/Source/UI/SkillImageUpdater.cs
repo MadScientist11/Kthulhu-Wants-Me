a367f42f49ce795f30b82697db7f7ee7e1d03a0c
Kyle <kcarpio98@hotmail.com>
2023-11-25 17:54:51 +0000

    Imported Graphy Package + Game Tweaks

---
+++ Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/UI/SkillImageUpdater.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using KthulhuWantsMe.Source.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KthulhuWantsMe.Source.Gameplay
+{
+    public class SkillImageUpdater : MonoBehaviour
+    {
+        public Sprite[] SkillImages;
+
+        
+        private void Update()
+        {
+            BranchView[] views = FindObjectsByType<BranchView>(FindObjectsSortMode.InstanceID);
+
+
+            if (views.Length > 0)
+            {
+                for (var i = 0; i < views.Length; i++)
+                {
+                    var branchView = views[i];
+                    Debug.Log(branchView.transform.GetChild(2).name, branchView.transform.GetChild(2));
+                    Image skillImage = branchView.transform.GetChild(2).GetComponent<UnityEngine.UI.Image>();
+
+                    skillImage.sprite = SkillImages[i];
+                }
+            }
+        }
+    }
+}
