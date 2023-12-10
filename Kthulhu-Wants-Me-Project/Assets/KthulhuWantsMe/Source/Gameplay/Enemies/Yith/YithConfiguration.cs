a5805851f2a5cbae033db2ceeba206a07d25a030
Alex Rogatch <quizandpuzzle@gmail.com>
2023-12-10 14:09:18 +0300

    Packages Clean Up

--- Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Gameplay/Enemies/Yith/YithConfiguration.cs
+++ Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Gameplay/Enemies/Yith/YithConfiguration.cs
@@ -1,4 +1,4 @@
-﻿using NaughtyAttributes;
+﻿using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Serialization;
 
--- Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Infrastructure/GameConfiguration.cs
+++ Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Infrastructure/GameConfiguration.cs
@@ -1,13 +1,11 @@
-﻿using KthulhuWantsMe.Source.Gameplay.Locations;
-using NaughtyAttributes;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace KthulhuWantsMe.Source.Infrastructure.Scopes
 {
     [CreateAssetMenu(menuName = "Create GameConfiguration", fileName = "GameConfiguration", order = 0)]
     public class GameConfiguration : ScriptableObject
     {
-        [Scene] public string UIScene;
-        [Scene] public string MainScene;
+        public string UIScene;
+        public string MainScene;
     }
 }
