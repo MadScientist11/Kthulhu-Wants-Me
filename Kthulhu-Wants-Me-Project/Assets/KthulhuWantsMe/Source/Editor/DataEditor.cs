0c9cb195c8fad5d69bb3c8dffb77d1d4e21ecf68
Alex Rogatch <quizandpuzzle@gmail.com>
2023-12-10 21:36:12 +0300

    Add Simple Data Editor

---
+++ Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Editor/DataEditor.cs
@@ -0,0 +1,21 @@
+﻿using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+
+namespace KthulhuWantsMe.Source.Editor
+{
+    public class DataEditor : OdinMenuEditorWindow
+    {
+        [MenuItem("Tools/Data Editor")]
+        private static void OpenWindow()
+        {
+            GetWindow<DataEditor>().Show();
+        }
+        
+        protected override OdinMenuTree BuildMenuTree()
+        {
+            OdinMenuTree menuTree = new OdinMenuTree();
+            menuTree.AddAllAssetsAtPath("Data", "Assets/KthulhuWantsMe/Data", true);
+            return menuTree;
+        }
+    }
+}
--- Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Gameplay/GameplayStateMachine/States/WaitForNextWaveState.cs
+++ Kthulhu-Wants-Me-Project/Assets/KthulhuWantsMe/Source/Gameplay/GameplayStateMachine/States/WaitForNextWaveState.cs
@@ -51,7 +51,6 @@ namespace KthulhuWantsMe.Source.Gameplay.GameplayStateMachine.States
             {
                 await UniTask.Delay(1000);
                 countdown--;
-                //_uiService.MiscUI.UpdateWaveCountdownText(countdown);
                 if (countdown == 0)
                 {
                     cancellationToken.Cancel();
